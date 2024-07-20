3af53c7af400f963e7fab1a6ad1914b004ab6fcb
sergiogalici <103759691+sergiogalici@users.noreply.github.com>
2024-07-20 10:45:19 +0200

    Update exercise1.test.js

--- test/exercise1.test.js
+++ test/exercise1.test.js
@@ -6,7 +6,7 @@ describe("Exercise 1: Kill Creatures", () => {
   })
 
   test("Kills all but one troll with almost sufficient arrows", () => {
-  expect(killCreatures(5, 3, 2, 20)).toEqual([5, 3, 1, 0])
+  expect(killCreatures(5, 3, 2, 20)).toEqual([5, 3, 1, 4])
   })
 
   test("Kills only rats and goblins with limited arrows", () => {
